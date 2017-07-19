4dbcb6f4f71707d0e147e51416be710ed63d26fa
Chemiclkid <chemica7x@gmail.com>
2017-07-19 13:57:57 -0700

    Create Part1.py

---
+++ Game/Part1.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+print("Welcome to legacy evolved")
+print("Before we begin, we would like to ask you some questions")
+pause
+firstName = input("What is your first name: ")
+
+//* The tv turns on in game and breaking news occur, The zombie apocalypse has started*\\
+print("Your family panics and they start running outside")
+
+//* The player recalls that there was a plan set during middle school for all survivors to meet up at your old school*\\
+//* At this point the player will have 10 minutes to scavange what he can from around the premise*\\
